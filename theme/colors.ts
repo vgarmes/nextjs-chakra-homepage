a35cefcae898bcb27aa23ca209dec78f358c1168
Victor Garcia Mestre <vgmestre@gmail.com>
2021-12-22 20:16:05 +0100

    add more colors

--- theme/colors.ts
+++ theme/colors.ts
@@ -1,10 +1,17 @@
 import { Colors } from '@chakra-ui/theme'
+import { theme as baseTheme } from '@chakra-ui/react'
+
+const { colors: baseColors } = baseTheme
 
 const colors: Colors = {
   black: '#16161D',
   background: {
     light: '#f0e7db',
     dark: '#202023'
+  },
+  subtleBackground: {
+    light: baseColors.whiteAlpha['500'],
+    dark: baseColors.whiteAlpha['200']
   },
   glassTeal: '#88ccca'
 }
